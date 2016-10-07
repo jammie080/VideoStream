f3dc14108f4136334f219fa248b15cb1dbf92796
jammie messam <diycertified@aol.com>
2016-10-07 16:59:16 -0400

    Added Files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         VideoStreame
+// @namespace    https://greasyfork.org/users/
+// @version      0.0.1
+// @description  Replaces the stream of videos on varies websites. This project was for fun.
+// @author       J. Messam
+// @icon         http://kissanime.to/Content/images/favicon.ico
+// @match        http://kissanime.com/Anime/*
+// @match        http://kissanime.to/Anime/*
+// @match        https://kissanime.to/Anime/*
+// @match        https://*.googlevideo.com/*
+// @match        https://*.c.docs.google.com/*
+// @match        http://kissanime.to/Special/AreYouHuman*
+// @match        http://kissanime.com/Special/AreYouHuman*
+// @match        https://kissanime.to/Special/hi
+// @match        https://kissanime.to/Special/AreYouHuman*
+// @license      Creative Commons; http://creativecommons.org/licenses/by/4.0/
+// @require      http://code.jquery.com/jquery-1.11.0.min.js
+// @grant        none
+// ==/UserScript=
